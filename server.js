07c45743e8436857f05fa531ef0e9eada188af24
Tavan Thiry <tavanthiry@Arilds-Mac-mini.local>
2025-07-29 15:39:19 +0200

    added dependencies, imports and js code

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 import cors from "cors"
 import express from "express"
+import listEndpoints from "express-list-endpoints"
 
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
+import flowerData from "./data/flowers.json"
+
 const port = process.env.PORT || 8080
 const app = express()
 
@@ -13,7 +13,52 @@ app.use(express.json())
 
 // Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!")
+const endpoints = listEndpoints(app)
+res.json({
+  message: "Welcome to the Flower API",
+  endpoints: endpoints
+})
+
+})
+
+// endpoint for getting all flowers
+app.get ("/flowers", (req, res) => {
+  const { color, botanicalFamily } = req.query
+
+  let filteredFlowers = flowerData
+
+  if (color) {
+    filteredFlowers = filteredFlowers.filter(flower => flower.color.toLowerCase() === color.toLowerCase())
+  }
+
+  res.json(filteredFlowers)
+})
+
+/* Checking scent instead of color with ?scent=sweet
+app.get ("/flowers", (req, res) => {
+  const { scent, botanicalFamily } = req.query
+
+  let filteredFlowers = flowerData
+
+  if (scent) {
+    filteredFlowers = filteredFlowers.filter(flower => flower.scent.toLowerCase() === scent.toLowerCase())
+  }
+
+  res.json(filteredFlowers)
+})
+*/
+
+// endpoint for getting one flower
+app.get ("/flowers/:id", (req, res) => {
+
+  const flower = flowerData.find((flower) => flower.id === +req.params.id)
+
+// tiny error handling if we get an id that doesnt exist in our data
+  if (!flower) {
+    return res.status(404).json({ error: "flower not found" })
+  }
+
+  res.json(flower)
 })
 
 // Start the server
