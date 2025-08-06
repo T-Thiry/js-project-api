0e9b9f0145b5fb14ce317fe39dc7e6c8d39d52cb
Tavan Thiry <tavanthiry@Arilds-Mac-mini.local>
2025-08-06 21:18:16 +0200

    Add PATCH /thoughts/:id/like endpoint

--- routes/thoughtRoutes.js
+++ routes/thoughtRoutes.js
@@ -65,3 +65,37 @@ router.get("/thoughts/:id", async (req, res) => {
     })
   }
 })
+
+
+// PATCH - Like a thought
+router.patch("/thoughts/:id/like", async (req,res) => {
+  const { id } = req.params
+ 
+  try {
+    const updatedThought = await Thought.findByIdAndUpdate(
+      id,
+      { $inc: { hearts: 1 }}, // increase like by 1
+      { new: true }
+    )
+     if (!updatedThought) {
+      return res.status(404).json({
+        success: false,
+        response: [],
+        message: "Thought not found."
+      })
+    }
+ 
+    res.status(200).json({
+      success: true,
+      response: updatedThought,
+      message: "Thought liked successfully."
+    })
+ 
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      response: error,
+      message: "Internal server error! Failed to like the thought."
+    })
+  }
+})
