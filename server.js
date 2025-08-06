f769b97cc66aecb974301934532004a424bc95cf
Tavan Thiry <tavanthiry@Arilds-Mac-mini.local>
2025-08-06 20:42:48 +0200

    moved code to thoughtRoutes file

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ import userRoutes from "./routes/userRoutes"
 
 dotenv.config()
 
+
 // Connect to Mongo DB database
 const mongoURL = process.env.MONGO_URL || "mongodb://localhost/happy-thoughts-api"
 mongoose.connect(mongoURL)
@@ -15,9 +16,11 @@ mongoose.connect(mongoURL)
 const port = process.env.PORT || 8080
 const app = express()
 
+
 // Add middlewares to enable cors and json body parsing. 
 app.use(cors())
 app.use(express.json())
+
 
 // Seed database
 if (process.env.RESET_DB) {
@@ -40,49 +43,9 @@ app.get("/", (req, res) => {
   })
 })
 
-app.use("/users", userRoutes)
 app.use("/thoughts", thoughtRoutes)
+app.use("/users", userRoutes)
 
-
-// Endpoint for getting all flowers and should be called /flowers. We're using RESTFUL APIs.
-app.get ("/flowers", (req, res) => {
-  const { color, botanicalFamily } = req.query
-
-  let filteredFlowers = flowerData
-
-  if (color) {
-    filteredFlowers = filteredFlowers.filter(flower => flower.color.toLowerCase() === color.toLowerCase())
-  }
-
-  res.json(filteredFlowers)
-})
-
-/* Checking scent instead of color with ?scent=sweet
-app.get ("/flowers", (req, res) => {
-  const { scent, botanicalFamily } = req.query
-
-  let filteredFlowers = flowerData
-
-  if (scent) {
-    filteredFlowers = filteredFlowers.filter(flower => flower.scent.toLowerCase() === scent.toLowerCase())
-  }
-
-  res.json(filteredFlowers)
-})
-*/
-
-// endpoint for getting one flower
-app.get ("/flowers/:id", (req, res) => {
-
-  const flower = flowerData.find((flower) => flower.id === +req.params.id)
-
-// tiny error handling if we get an id that doesnt exist in our data
-  if (!flower) {
-    return res.status(404).json({ error: "flower not found" })
-  }
-
-  res.json(flower)
-})
 
 // Start the server
 app.listen(port, () => {
